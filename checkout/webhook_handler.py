6070dd29bf4c377c54dfcc9d2f89e6f937f342cd
noreen <noreennelligancilms@gmail.com>
2023-03-16 09:22:02 +0000

    update code in checkout app

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -7,7 +7,6 @@ from profiles.models import UserProfile
 import json
 import time
 
-
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
 
@@ -40,11 +39,11 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
-        # Update profile information if save_info was checked 
-        profile = None 
+        # Update profile information if save_info was checked
+        profile = None
         username = intent.metadata.username
         if username != 'AnonymousUser':
-            profile = UserProfile.objects.get(user_username=username)
+            profile = UserProfile.objects.get(user__username=username)
             if save_info:
                 profile.default_phone_number = shipping_details.phone
                 profile.default_country = shipping_details.address.country
@@ -54,7 +53,6 @@ class StripeWH_Handler:
                 profile.default_street_address2 = shipping_details.address.line2
                 profile.default_county = shipping_details.address.state
                 profile.save()
-
 
         order_exists = False
         attempt = 1
